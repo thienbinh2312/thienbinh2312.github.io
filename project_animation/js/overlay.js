0d412fe26125796d841dd7356fd2bd6437857f6b
thienbinh2312 <y18009@sangi.jp>
2020-05-30 00:21:28 +0900

    sua

---
+++ project_animation/js/overlay.js
@@ -0,0 +1,57 @@
+// JavaScript Document
+(function($){
+	"use strict";
+$(function(){
+
+	$('#toggle').click(function() {
+		$(this).toggleClass('toggle-active');
+		$('#overlay').toggleClass('nav-active');
+	});
+	$("#overlay").click(function(){
+		$("#toggle").removeClass("toggle-active");
+		$('#overlay').toggleClass('nav-active');
+	});
+	
+	//ページトップへ戻る
+	
+	$('.pnavi a').click(function () {
+				$('body,html').animate({			
+				scrollTop: 0
+				}, 400);
+				return false;
+	});
+	//slider
+	jQuery(function($){
+    $('.slider').each(function(){
+		$(this).slick({
+			autoplay:false,
+			autoplaySpeed:500,
+			dots:true,
+			/*slidesToShow:2,*/
+		});
+	});	
+});	
+		
+});
+})(jQuery);
+
+(function($){
+		"use strict";
+	$(function(){
+	$(window).scroll(function(){
+				var windowHeight = $(window).height();
+				var topWindow = $(window).scrollTop();
+				var blockAni = $('.parallax');
+				blockAni.each(function(){
+					var targetPosition = $(this).offset().top;
+					if(topWindow + windowHeight > targetPosition + 100){
+						$(this).addClass("css_show");
+					}
+					else {
+						$(this).removeClass('css_show');
+					}
+				});
+	});		
+});
+})(jQuery);
+ 
